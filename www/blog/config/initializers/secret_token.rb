4ae677d13f02cbb7ed4cf7bff8008f9c5216fdb3
Ubuntu <ubuntu@ip-172-31-12-102.eu-west-1.compute.internal>
2013-06-27 13:17:56 +0000

    Added existing Ruby on Rails project.

---
+++ www/blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '049b4d5145c1f769bb3b491a55c2bb568169471860723eee1ce961a7cd4aa4415fd7b42b9029ad44b541e68ee84e5413785affa894d697c63829f340f42cec12'
